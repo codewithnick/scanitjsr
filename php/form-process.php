4ffff358275ff88fada5fce340c14548ae7ecd91
codewithnick <nikhilsingh526452@gmail.com>
2023-03-04 22:07:20 +0530

    updated email

--- php/form-process.php
+++ php/form-process.php
@@ -51,7 +51,7 @@ $Bodytosend.=$Body;
 $Bodytosend.="\nWe will soon contact you back with an appropriate response";
 // send email
 //$success = mail($EmailTo, $Subject, $Body, "From:".$email);
-$success=sendamail("recruit.my.freelancer@gmail.com",$Subject,$Body);
+$success=sendamail("society.developers.2023@gmail.com",$Subject,$Body);
 $success=sendamail($email,"Response recieved",$Bodytosend);
 // redirect to success page
 if ($success){
@@ -75,13 +75,13 @@ function sendamail($to,$subject,$content){
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         //$mail->SMTPDebug=3;
-        $mail->Username   = 'recruit.my.freelancer@gmail.com';                     //SMTP username
-        $mail->Password   = 'tzcodlzvdxhamyua';                               //SMTP password
+        $mail->Username   = 'society.developers.2023@gmail.com';                     //SMTP username
+        $mail->Password   = 'exavhvotiycqendz';                               //SMTP password
         //$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
         $mail->SMTPSecure='tls';
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         //Recipients
-        $mail->setFrom('recruit.my.freelancer@gmail.com', 'Society Of Computer Applications');
+        $mail->setFrom('society.developers.2023@gmail.com', 'Society Of Computer Applications');
         $mail->addAddress($to);     //Add a recipient
        // $mail->addReplyTo('info@gmail.com', 'Information');
         //$mail->addCC('cc@gmail.com');
